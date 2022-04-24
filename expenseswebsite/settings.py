bcfd9d380fbf98f956da09172830868ccbc830a8
Zayn Hamza <nerovsdante89@gmail.com>
2022-04-24 17:56:41 +0300

    commented middleware whitenoise

--- expenseswebsite/settings.py
+++ expenseswebsite/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    # 'whitenoise.storage.CompressedManifestStaticFilesStorage',
 ]
 
 ROOT_URLCONF = 'expenseswebsite.urls'
